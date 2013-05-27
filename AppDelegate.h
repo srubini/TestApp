5a4a07635ac1c063650354933b59ffb8c654d06f
SRubini <srubini@prokarmasoftech.com>
2013-05-27 12:08:44 +0530

    First Commit

---
+++ AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TestApp
+//
+//  Created by Rubini on 5/27/13.
+//  Copyright (c) 2013 Prokarma. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
